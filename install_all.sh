10efcc20836a3922eb9cf9bb60a9468081485bcd
Léo Flaventin Hauchecorne <hl037.prog@gmail.com>
2018-10-18 08:31:48 +0000

    .

--- install_all.sh
+++ install_all.sh
@@ -31,7 +31,7 @@ echo "
 ln -s "$o/.vim" "$HOME/"
 mkdir -p "$HOME/.config"
 cp -r nvim "$HOME/.config"
-ln -s "$HOME/.vimrc" "$HOME/.vim/.vimrc"
+ln -s "$HOME/.vim/.vimrc" "$HOME/.vimrc" 
 cd "$HOME/.vim/dein/"; ./installer.sh .
 /usr/bin/vim "+call dein#install()" "+q"
 /usr/bin/nvim "+call dein#install()" "+q"
