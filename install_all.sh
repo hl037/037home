a2a3e4e9b9020bc98fba81b922a2f06d8194f4c5
Léo Flaventin Hauchecorne <hl037.prog@gmail.com>
2018-10-18 08:04:43 +0000

    + nvim boiler plate

--- install_all.sh
+++ install_all.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 
+function msg(){
+   set +x
+   set +v
+   
+   echo $@
+   
+   set -x
+   set -v
+}
+
+
+set -x
+set -v
+
 o=`pwd`
 
 #Base Home
+msg "
+ ###########
+# Base Home #
+ ###########
+"
 install "$o/home/*" "$HOME"
 
 #Oh-My-Zsh
+msg "
+ ###########
+# Oh-My-ZSH #
+ ###########
+"
 cd zsh_conf; ./install.sh
 
 #vim
+msg "
+ ###########
+#   Vimrc   #
+ ###########
+"
 ln -s "$o/.vim" "$HOME/"
 cd "$HOME/.vim/dein/"; ./installer.sh
-vim +"call dein#install()" +q
+mkdir -p "$HOME/.config"
+cp -r nvim "$HOME/.config"
+/usr/bin/vim +"call dein#install()" +q
+/usr/bin/nvim +"call dein#install()" +q
 
-
+set +x
+set -x
 
