1956801d536d3d0032531b03b414fe79f64d3927
Léo Flaventin Hauchecorne <hl037.prog@gmail.com>
2019-07-16 15:32:27 +0200

    f synthax-highlighting

--- zsh_conf/install.sh
+++ zsh_conf/install.sh
@@ -4,6 +4,7 @@ p=$(pwd)
 g="$HOME/.local/share/oh-my-zsh"
 git clone https://github.com/robbyrussell/oh-my-zsh.git "$g"
 cd "$g"
+cd custom/plugins
 git submodule add https://github.com/zsh-users/zsh-syntax-highlighting.git
 install $p/muse-leo.zsh-theme "$g"/themes/muse-leo.zsh-theme 
 
