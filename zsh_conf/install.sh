6d97af66e75cb142981456e5d9873e9589c98cd3
Léo Flaventin Hauchecorne <hl037.prog@gmail.com>
2019-08-05 11:19:17 +0200

    f zsh : missing -f git submodule add synthax-highlighting

--- zsh_conf/install.sh
+++ zsh_conf/install.sh
@@ -5,7 +5,7 @@ g="$HOME/.local/share/oh-my-zsh"
 git clone https://github.com/robbyrussell/oh-my-zsh.git "$g"
 cd "$g"
 cd custom/plugins
-git submodule add https://github.com/zsh-users/zsh-syntax-highlighting.git
+git submodule add -f https://github.com/zsh-users/zsh-syntax-highlighting.git
 install $p/muse-leo.zsh-theme "$g"/themes/muse-leo.zsh-theme 
 
 install $p/.profile $HOME
